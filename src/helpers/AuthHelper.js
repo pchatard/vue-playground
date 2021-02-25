63c2aee9714798d1452eece6fcdc487d139d0863
pchatard <chatard.pierre@gmail.com>
2021-02-25 21:45:10 +0100

    Production configuration

--- src/helpers/AuthHelper.js
+++ src/helpers/AuthHelper.js
@@ -1,18 +1,11 @@
 import Vue from "vue";
 import firebase from "firebase/app";
-import { authorizedUsersDev } from "@/data/config";
-
-const authorizedUsersProd = process.env.AUTH_USER_IDS.split(",");
 
 export class Auth {
 	static initializeAuthState(commit) {
-		const authorizedUsers =
-			process.env.NODE_ENV === "production"
-				? authorizedUsersProd
-				: authorizedUsersDev;
+		const authUsers = process.env.VUE_APP_AUTHORIZED_USER.split(" ");
 		firebase.auth().onAuthStateChanged((user) => {
-			// TODO: Change condition to user id with environment variable
-			if (user && authorizedUsers.includes(user.uid)) {
+			if (user && authUsers.includes(user.uid)) {
 				const formatedUser = {
 					name: user.displayName,
 					email: user.email,
@@ -21,7 +14,7 @@ export class Auth {
 				};
 				commit("INITIALIZE", formatedUser);
 				return;
-			} else if (user && !authorizedUsers.includes(user.uid)) {
+			} else if (user && !authUsers.includes(user.uid)) {
 				Vue.prototype.$notify.error({
 					title: "Authorization Error",
 					message: "You are not authorized",
--- src/helpers/Firebase.js
+++ src/helpers/Firebase.js
@@ -1,21 +1,9 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-import config from "../data/config";
-
-const configProd = {
-	apiKey: process.env.API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECT_ID,
-	storageBucket: process.env.STORAGE_BUCKET,
-	messagingSenderId: process.env.MESSAGING_SENDER_ID,
-	appId: process.env.APP_ID,
-	databaseURL: process.env.DATABASE_URL,
-};
+import config from "../config/firebase";
 export class FirebaseHelper {
-	static init() {
-		firebase.initializeApp(
-			process.env.NODE_ENV === "production" ? configProd : config
-		);
+	static async init() {
+		firebase.initializeApp(config);
 	}
 }
